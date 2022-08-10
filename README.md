f47bd6daa0da5a1ee35bb2765d03afa64bac493a
echils <echils@outlook.com>
2022-08-10 15:27:08 +0800

    project init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # knowledge-accumulation-golang
 Record the related knowledge accumulated in the daily learning and work of Golang
+
+<p align="center">
+  <a>
+   <img alt="Framework" src="ECHILS.PNG">
+  </a>
+</p>
---
+++ src/helloworld.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("========================")
+	fmt.Println("Welcome to Golang world")
+}
+
+func main() {
+
+	var content = "I am a developer"
+	fmt.Println(content)
+
+}
