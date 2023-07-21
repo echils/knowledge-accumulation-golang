f1dda1dd96efa37364d77dfad49ac35d2b0b7070
echils <echils@outlook.com>
2023-07-21 20:49:37 +0800

    load config file

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"web/pkg/setting"
+)
+
+// 环境变量相对路径
+const configFile = "./config/app.yaml"
+
 func main() {
+
+	//项目环境配置引导
+	b, _ := setting.Bootstrap(configFile)
+	if !b {
+		panic("fatal error when bootstrap")
+	}
 
 }
